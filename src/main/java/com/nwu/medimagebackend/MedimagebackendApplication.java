97b85d14190fa54c3bb538856f4f1e5a92d2b389
WangjiaweiY <127864206+WangjiaweiY@users.noreply.github.com>
2024-12-25 14:17:46 +0800

    initial project

---
+++ src/main/java/com/nwu/medimagebackend/MedimagebackendApplication.java
@@ -0,0 +1,13 @@
+package com.nwu.medimagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedimagebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedimagebackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nwu/medimagebackend/MedimagebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwu.medimagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimagebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
