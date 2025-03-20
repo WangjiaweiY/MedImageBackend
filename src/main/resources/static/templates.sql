a7920f82bbae7702beb6a787dba6905c0d5188e3
WangjiaweiY <127864206+WangjiaweiY@users.noreply.github.com>
2025-03-20 22:31:59 +0800

    新增sql模板语句

---
+++ src/main/resources/static/templates.sql
@@ -0,0 +1,24 @@
+create table ihc_analysis_result
+(
+    id             int auto_increment
+        primary key,
+    image_name     varchar(255)                            not null,
+    positive_area  bigint                                  not null,
+    total_area     bigint                                  not null,
+    analysis_date  timestamp     default CURRENT_TIMESTAMP not null,
+    foldername     varchar(255)                            not null,
+    positive_ratio decimal(5, 2) default 0.00              not null,
+    constraint unique_folder_image
+        unique (foldername, image_name)
+);
+
+create table users
+(
+    id       int auto_increment
+        primary key,
+    username varchar(50)  not null,
+    password varchar(255) not null,
+    constraint username
+        unique (username)
+);
+
